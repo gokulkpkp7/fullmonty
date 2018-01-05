3349c46b108f37838ad6c00d721385bb070ea7b0
gokul <gokulkp.kp7@gmail.com>
2018-01-05 10:36:56 +0530

    first commit

---
+++ application/views/admin_login_view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-3 col-md-3">
+				<form>
+					<div class="form-group">
+				    	<label for="email">Email address:</label>
+				    	<input type="email" class="form-control" id="email" autofocus>
+					</div>
+				  	<div class="form-group">
+				    	<label for="pwd">Password:</label>
+				    	<input type="password" class="form-control" id="pwd">
+				  	</div>
+				  	<button type="submit" class="btn btn-basic">Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
